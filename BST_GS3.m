a08c521821be1571302fde2376c1edd2a3f839c5
Adam Hockley <66746704+adam-hockley@users.noreply.github.com>
2024-03-28 14:49:57 +0100

    Update BST_GS3.m

--- BST_GS3.m
+++ BST_GS3.m
@@ -1,10 +1,3 @@
-function [Spikes] = BST_GS4(bst,trials,ch)
+function [Spikes] = BST_GS3(bst,trials,un)
 
-% temp = bst.Spikes(bst.Spikes.chan==ch,:);
-% Spikes = temp.('RasterSW')(ismember(temp.TrialIdx,trials));
-
-Spikes = bst.Spikes.('RasterSW')(ismember(bst.Spikes.TrialIdx,trials) & bst.Spikes.chan==ch );
-
-% testing here
-% find(ismember(bst.Spikes.TrialIdx,trials) & bst.Spikes.chan==ch )
-
+Spikes = bst.Spikes.('RasterSW')(ismember(bst.Spikes.TrialIdx,trials) & bst.Spikes.unit==un );
